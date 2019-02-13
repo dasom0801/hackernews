ab99b01e317ac01d6cfd03a1a9d2f90c06734c07
Dasom Kim <dev.dasom1012@gmail.com>
2019-02-13 16:36:16 +0900

    Fix : 컴포넌트 분리

--- src/App.js
+++ src/App.js
@@ -50,14 +50,45 @@ class App extends Component {
     const { searchTerm, list} = this.state;
     return (
       <div className="App">
-          <form>
-            <input 
-              type="text"
-              value={searchTerm} 
-              onChange={this.onSearchChange}
-            />
-          </form>
-          {list.filter(isSearched(searchTerm)).map(item => 
+          <Search 
+            value={searchTerm}
+            onChange={this.onSearchChange}
+          >
+            Search
+          </Search>
+          <Table 
+            list={list}
+            pattern={searchTerm}
+            onDismiss={this.onDismiss}
+          />
+      </div>
+    );
+  }
+}
+
+class Search extends Component {
+  render() { 
+    const {value, onChange, children} = this.props;
+    return ( 
+      <form>
+        {children}
+        <input
+          type="text"
+          value={value}
+          onChange={onChange}
+        />
+      </form>
+    );
+  }
+}
+
+class Table extends Component {
+  render() { 
+    const { list, pattern, onDismiss }  = this.props;
+    return ( 
+      <div>
+        {
+          list.filter(isSearched(pattern)).map(item =>
             <div key={item.objectID}>
               <span>
                 <a href={timeout.url}>{item.title}</a>
@@ -66,13 +97,35 @@ class App extends Component {
               <span>{item.num_comments}</span>
               <span>{item.points}</span>
               <span>
-                <button onClick={() => this.onDismiss(item.objectID)}>dismiss</button>
+                <Button onClick={() => onDismiss(item.objectID)}>
+                  Dismiss
+                </Button>
               </span>
             </div>
-          )}
+          )
+        }
       </div>
-    );
+     );
   }
 }
 
+class Button extends Component {
+  render() {
+    const {
+      onClick,
+      className = '',
+      children,
+    } = this.props;
+    return ( 
+      <button
+        onClick={onClick}
+        className={className}
+        type="button"
+      >
+        {children}
+      </button>
+     );
+  }
+}
+ 
 export default App;

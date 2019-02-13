f61dd3dde91b3a8dc4033ee44e73cff80fd2103e
Dasom Kim <dev.dasom1012@gmail.com>
2019-02-13 16:00:14 +0900

    Add list search

--- src/App.js
+++ src/App.js
@@ -1,25 +1,75 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { timeout } from 'q';
+
+const list = [
+  {
+    title: 'React',
+    url: 'https://reactjs.org/',
+    author: 'Jordan Walke',
+    num_comments: 3,
+    points: 4,
+    objectID: 0,
+  },
+  {
+    title: 'Redux',
+    url: 'https://github.com/reactjs/redux',
+    author: 'Dan Abramov, Andrew Clark',
+    num_comments: 2,
+    points: 5,
+    objectID: 1,
+  },
+];
+
+const isSearched = searchTerm => item =>
+  item.title.toLowerCase().includes(searchTerm.toLowerCase());
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      list,
+      searchTerm: '',
+    };
+    
+    this.onDismiss = this.onDismiss.bind(this);
+    this.onSearchChange = this.onSearchChange.bind(this);
+
+  }
+  
+  onDismiss(id) {
+    const updatedList = this.state.list.filter(item => item.objectID !== id);
+    this.setState({list: updatedList});
+  }
+
+  onSearchChange(event) {
+    this.setState({ searchTerm: event.target.value})
+  }
+  
   render() {
+    const { searchTerm, list} = this.state;
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+          <form>
+            <input 
+              type="text"
+              value={searchTerm} 
+              onChange={this.onSearchChange}
+            />
+          </form>
+          {list.filter(isSearched(searchTerm)).map(item => 
+            <div key={item.objectID}>
+              <span>
+                <a href={timeout.url}>{item.title}</a>
+              </span>
+              <span>{item.author}</span>
+              <span>{item.num_comments}</span>
+              <span>{item.points}</span>
+              <span>
+                <button onClick={() => this.onDismiss(item.objectID)}>dismiss</button>
+              </span>
+            </div>
+          )}
       </div>
     );
   }
--- src/index.js
+++ src/index.js
@@ -6,6 +6,12 @@ import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
+// Hot Module Replacement 추가
+// 새로고침 없이 애플리케이션을 재실행
+if(module.hot) {
+  module.hot.accept();
+}
+
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: http://bit.ly/CRA-PWA
